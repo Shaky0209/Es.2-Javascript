c6210a01c3ee6a4b942cfdb24d65266bbb9ceae8
Andrea Busini <shaky@email.it>
2023-11-15 18:55:05 +0100

    first commit

---
+++ S2P1-scaricabile.js
@@ -0,0 +1,252 @@
+//ESERCIZI SUGLI IF: 
+
+/* ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/*
+
+let a = 10;
+let b = 100;
+
+if(a > b){
+  console.log("Il numero maggiore è contenuto in a, ed è uguale a " + a);
+}else{
+  console.log("Il numero maggiore è contenuto in b, ed è uguale a " + b);
+}
+
+*/
+
+/*
+ESERCIZIO 2
+  Crea un blocco condizionale if/else per mostrare in console il messaggio corretto in ogni condizione.
+
+  num < 5 - mostra in console "Tiny"
+  num < 10 - mostra in console "Small"
+  num < 15 - mostra in console "Medium"
+  num < 20 - mostra in console "Large"
+  num >= 20 - mostra in console "Huge"
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+
+/*
+
+/*
+CON QUESTO METODO E LE CONOSCENZE ACQUISITE NON RIESCO A FILTRARE I NUMERI BOOLEANI, LE STRINGHE SI, PER IL MOMENTO LO LASCIO COSì,
+HO PROVATO A RISOLVERE IL PROBLEMA UTILIZZANDO SOTTO L' IF/ELSE CHE E' UN CICLO CHE A QUANTO PARE E' VERIFICATO PER UNA MAGGIORE SICUREZZA.
+*/
+
+/*
+let num = 10;
+
+switch(num <= 0, num > 0, num !== false, num !== true){
+
+  case num === 0 && test === NaN:
+    console.log("Tiny");
+    break;
+
+    case num < 5 :
+    console.log("Tiny");
+    break;
+
+  case num < 10:
+    console.log("Small");
+    break;
+
+  case num < 15:
+    console.log("Medium");
+    break;
+
+  case num < 20:
+    console.log("Large");
+    break;
+
+  case num > 20:
+    console.log("Huge");
+    break;
+
+  default:
+    console.log("The value was entered incorrectly.");
+}
+
+*/
+
+/*
+
+let num = 10;
+
+if(num === null){
+  console.log("The value is null.");
+}else if(num === false || num === true){
+  console.log("The boolean value are'nt accepted.");
+}else  if(num < 5){
+  console.log("Tiny");
+}else if(num < 10){
+  console.log("Small");
+}else if(num < 15){
+  console.log("Medium");
+}else if(num < 20){
+  console.log("Large");
+}else if(num >= 20){
+  console.log("Huge");
+}else{
+  console.log("The value was entered incorrectly.");
+}
+
+*/
+
+
+//ESERCIZI SUI CICLI: 
+
+/* ESERCIZIO 3
+  Mostra i numeri da 0 a 10 (incluso) in ordine ascendente, ma evitando di mostrare i numeri 3 e 8 (suggerimento: ripassa l'uso
+  di "continue").
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/*
+for (i = 0; i <= 10; i++){
+    
+  if(i === 3 || i === 8){
+      continue;
+  }
+    
+  console.log(i);
+}
+
+*/
+
+/* ESERCIZIO 11
+  Scrivi un ciclo in JavaScript per iterare da 0 a 15. Per ciascun elemento, il ciclo deve controllare che il valore corrente sia pari o
+  dispari, e mostrare il risultato in console.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/*
+
+for(i = 0; i <= 15; i++){
+  if((i % 2) === 0){
+    console.log(i + " è un numero paro");
+  }else{
+    console.log(i + " è un numero disparo");
+  }
+}
+
+*/
+
+//ESERCIZI EXTRA NON OBBLIGATORI
+
+/* ESERCIZIO EXTRA 1
+  Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione
+ sia uguale a 8.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/*
+
+let a = 1;
+let b = 9;
+
+if(a === 8){
+  console.log("a è uguale a 8");
+}else if (b === 8){
+  console.log("b è uguale a 8");
+}else if((a - b) === 8){
+  console.log("a = " + a + " -  b = " + b + " ha come risultato 8");
+}else if((a + b) === 8){
+  console.log("a = " + a + " + b = " + b + " ha come risultato 8");
+}else if((b - a) === 8){
+  console.log("b = " + b + " - a = " + a + " ha come risultato 8");
+}else{
+  console.log("Sia a = " + a + " che  b = " + b + " sono diversi da 8, la loro sottrazione è diversa da 8 e la loro somma è diversa da 8");
+}
+
+*/
+
+/* ESERCIZIO EXTRA 2
+Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
+C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione
+ha un costo fisso pari a 10).
+Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+
+/*
+let totalShoppingCart = 51;
+
+if (totalShoppingCart <= 50){
+  totalShoppingCart = totalShoppingCart + 10;
+  console.log("Il totale del tuo ordine è di € " + totalShoppingCart);
+}else{
+  console.log("il totale del tuo ordine è di € " + totalShoppingCart);
+}
+
+*/
+
+/* ESERCIZIO EXTRA 3
+  Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
+  Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando, usando l'algoritmo del codice 
+  precedente, se le spedizioni siano gratuite oppure no e e calcolando il totale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/*
+
+let totalShoppingCart = 62;
+
+if ((totalShoppingCart * 0.8) <= 50){
+  totalShoppingCart = totalShoppingCart + 10;
+  console.log("Il totale del tuo ordine è di € " + totalShoppingCart);
+}else{
+  console.log("Il totale del tuo ordine è di € " + totalShoppingCart);
+}
+
+*/
+
+/*  ESERCIZIO EXTRA 4
+  Usa un operatore ternaio per assegnare ad una variabile chiamata "gender" i valori "male" o "female".
+  La scelta deve essere basata sul valore di un'altra variabile booleana chiamata isMale.
+  Es. se isMale e' vero, il valore di gender deve essere "male"
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/*
+
+let isMale = true ? gender = "male" : gender = "female";
+
+console.log(gender);
+
+*/
+
+/* ESERCIZIO EXTRA 5
+  Scrivi un algoritmo che iteri i numeri da 1 a 100, stampandoli in console. Se un valore tuttavia è multiplo di 3 (operatore modulo!),
+  stampa al suo posto la parola "Fizz" e se il numero è multiplo di 5, stampa "Buzz". Se le condizioni si verificano entrambe,
+  stampa "FizzBuzz".
+*/
+
+/*
+
+for(i = 1; i <= 100; i++){
+  if((i % 3) === 0 && (i % 5) === 0){
+    console.log(i + " FizzBuzz");
+  }else if((i % 3) === 0){
+    console.log(i + " Fizz");
+  }else if((i % 5) === 0){
+    console.log(i + (" Buzz"));
+  }else{
+    console.log(i);
+  }
+}
+
+*/
